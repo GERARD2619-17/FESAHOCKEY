bc3f780de29f72309f8af709cd80343cc07d6425
GERARD2619-17 <gera121134@gmail.com>
2024-08-21 13:10:18 -0600

    primercommit

---
+++ app/Http/Controllers/JugadoresController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\jugadores;
+use Illuminate\Http\Request;
+
+class JugadoresController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(jugadores $jugadores)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(jugadores $jugadores)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, jugadores $jugadores)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(jugadores $jugadores)
+    {
+        //
+    }
+}
